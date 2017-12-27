ce1a3ae88d55a1099b99a2128d7e97be1a9150b8
angarev <angarev@abv.bg>
2017-12-27 09:02:09 +0200

    Lesson 1v2

--- lesson_1/js/main.js
+++ lesson_1/js/main.js
@@ -15,6 +15,9 @@ new Vue({
         },
         dummy: function(event) {
             event.stopPropagation();
+        },
+        alertMe: function() {
+            alert('Alert!');
         }
     }
 });
